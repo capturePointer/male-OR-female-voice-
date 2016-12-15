5843948ad95781e2c4aa99162d229640160e7321
Krzysztof Jurkiewicz <huntekah@gmail.com>
2016-12-15 13:27:01 +0100

    going to unversity update

--- wav1.py
+++ wav1.py
@@ -22,14 +22,30 @@ def draw_plot(array, where=211, opis_xlabel="czas", opis_ylabel="czestotliwosc")
     ylabel(opis_ylabel)
     #show()
 
+def draw_spectrum(array, where=212, opis_xlabel="czas", opis_ylabel="czestotliwosc", cut = 100000):
+    subplot(where)
+    sgn = fft(array)
+    signal = abs(sgn)
+    # HOW TO GET FREQUENCIES?
+    for i,value in enumerate(signal):
+        if(value < cut):
+            signal[i] = 0
+            sgn[i] = 0
+    stem(range(len(signal)), signal, '-*')
+    return abs(ifft(sgn))
+
+
 if __name__ == "__main__":
-    w,signal = wav.read(WAV_FILE)
+    w, signal = wav.read(WAV_FILE)
     signal = [s[0] for s in signal] # pierwszy kanał to s[0]
     signal = signal[::10]
     draw_plot(signal)
-
+    array = draw_spectrum(signal, cut = 10000)
+    show()
+    draw_plot(signal)
+    draw_plot(array, where = 212)
     #subplot(211)
     #array = show_spots(SPOTS)
-    subplot(212)
+    #subplot(212)
     #show_spectrum(array)
     show()
